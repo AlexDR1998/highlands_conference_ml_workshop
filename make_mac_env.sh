f453d1a877f7d9fa084902d6096ec634b4796600
Alex <alexander.richardson@gmail.com>
2024-02-20 12:44:02 +0000

    removing mac bash script

--- make_mac_env.sh
+++
@@ -1,7 +0,0 @@
-conda create -n ml_workshop_mac python=3.11
-conda activate ml_workshop_mac
-pip install jaxlib jax equinox optax diffrax tqdm matplotlib tensorflow einops ipywidgets
-python setup.py
-conda env export --no-builds > env_mac_nb.yml
-conda env export > env_mac.ml
-conda deactivate
